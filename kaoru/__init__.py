34e744f7c12dd38699810829c41ee87fce448c46
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-12-21 19:45:29 -0430

    bump version number

--- kaoru/__init__.py
+++ kaoru/__init__.py
@@ -14,6 +14,6 @@ The very basics
 PKG_URL = 'https://github.com/axltxl/kaoru'
 __name__ = 'kaoru'
 __author__ = 'Alejandro Ricoveri'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __licence__ = 'MIT'
 __copyright__ = 'Copyright (c) Alejandro Ricoveri'
